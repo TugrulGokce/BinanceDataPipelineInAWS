227ef2423a866776b5212ccd9b4b58acd37ac381
Tugrul <tudi2882@gmail.com>
2023-03-14 12:20:49 +0300

    lambda_function updated.

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,20 @@ import mysql.connector
 import urllib.parse
 import boto3
 
-s3 = boto3.client('s3')
+s3 = boto3.client('s3', region_name="eu-north-1")
 
 
+# If you want to see and track lambda logs, go to CloudWatch and find stream log groups.
 def lambda_handler(event, context):
-    # Event içerisinden bucket'a her .tsv dosyası eklendiğinde dosyanın
-    # ismini al ve s3-url oluştur
-    bucket = event['Records'][0]['s3']['bucket']['name']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-    s3_url = f"s3://{bucket}/{key}"
+    print(f"{bucket_name=}")
+    print(f"{key=}")
+
+    s3_url = f"s3://{bucket_name}/{key}"
     print(s3_url)
 
-    # Aurora mysql baglantisi
     cnx = mysql.connector.connect(host=os.environ["RDS_HOSTNAME"], user=os.environ["RDS_USERNAME"],
                                   passwd=os.environ["RDS_PASSWORD"],
                                   database=os.environ["RDS_DB_NAME"], port=os.environ["RDS_PORT"])
@@ -31,6 +32,22 @@ def lambda_handler(event, context):
     cnx.commit()
     cnx.close()
 
-    return {"statusCode": 200,
-            "headers": {"content-type": "application/json"},
-            "body": json.dumps(cur.lastrowid, indent=4, sort_keys=True, default=str)}
+    print(f"{key.split('/')[-1]} succesfully inserted to RDS Table.")
+
+    # write copy statement
+    response = s3.copy_object(
+        Bucket=bucket_name,
+        CopySource={'Bucket': bucket_name, 'Key': key},
+        Key=f"data_finished/{key.split('/')[-1]}",
+    )
+
+    print(f"File '{key.split('/')[-1]}' copied to '{bucket_name}/data_finished/'")
+    print(response)
+
+    s3.delete_object(Bucket=bucket_name, Key=key)
+    print(f"File '{key.split('/')[-1]}' succesfully deleted from '{bucket_name}/data_1_min/'")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File has been Successfully Copied')
+    }
